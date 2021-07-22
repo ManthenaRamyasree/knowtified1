9c309c830dd92591f0398142a9568368d8ebc121
ramyasreemantheena800@gmail.com <ramyasreemanthena800@gmail.com>
2021-07-22 14:01:50 +0530

    new file:   .editorconfig new file:   .gitignore new file:   README.md new file:   angular.json new file:   e2e/protractor.conf.js new file:   e2e/src/app.e2e-spec.ts new file:   e2e/src/app.po.ts new file:   e2e/tsconfig.e2e.json new file:   package-lock.json new file:   package.json new file:   src/app/app-routing.module.ts new file:   src/app/app.component.css new file:   src/app/app.component.html new file:   src/app/app.component.spec.ts new file:   src/app/app.component.ts new file:   src/app/app.module.ts new file:   src/app/auth.service.spec.ts new file:   src/app/auth.service.ts new file:   src/app/models/list.model.ts new file:   src/app/models/task.model.ts new file:   src/app/pages/edit-list/edit-list.component.css new file:   src/app/pages/edit-list/edit-list.component.html new file:   src/app/pages/edit-list/edit-list.component.spec.ts new file:   src/app/pages/edit-list/edit-list.component.ts new file:   src/app/pages/edit-task/edit-task.component.html new file:   src/app/pages/edit-task/edit-task.component.scss new file:   src/app/pages/edit-task/edit-task.component.spec.ts new file:   src/app/pages/edit-task/edit-task.component.ts new file:   src/app/pages/new-list/new-list.component.css new file:   src/app/pages/new-list/new-list.component.html new file:   src/app/pages/new-list/new-list.component.spec.ts new file:   src/app/pages/new-list/new-list.component.ts new file:   src/app/pages/new-task/new-task.component.css new file:   src/app/pages/new-task/new-task.component.html new file:   src/app/pages/new-task/new-task.component.spec.ts new file:   src/app/pages/new-task/new-task.component.ts new file:   src/app/pages/task-view/task-view.component.css new file:   src/app/pages/task-view/task-view.component.html new file:   src/app/pages/task-view/task-view.component.spec.ts new file:   src/app/pages/task-view/task-view.component.ts new file:   src/app/task.service.spec.ts new file:   src/app/task.service.ts new file:   src/app/web-req.interceptor.ts new file:   src/app/web-request.service.spec.ts new file:   src/app/web-request.service.ts new file:   src/assets/.gitkeep new file:   src/assets/plus-icon.svg new file:   src/browserslist new file:   src/environments/environment.prod.ts new file:   src/environments/environment.ts new file:   src/favicon.ico new file:   src/index.html new file:   src/karma.conf.js new file:   src/main-styles.css new file:   src/main.ts new file:   src/polyfills.ts new file:   src/styles.css new file:   src/test.ts new file:   src/tsconfig.app.json new file:   src/tsconfig.spec.json new file:   src/tslint.json new file:   tsconfig.json new file:   tslint.json

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { WebReqInterceptor } from './web-req.interceptor';
+import { EditListComponent } from './pages/edit-list/edit-list.component';
+import { EditTaskComponent } from './pages/edit-task/edit-task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskViewComponent,
+    NewListComponent,
+    NewTaskComponent,
+    EditListComponent,
+    EditTaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: WebReqInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
